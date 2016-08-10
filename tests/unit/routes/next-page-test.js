2f796f648108550ffda0dea353f0b8828d2f8b26
Chuabacca <jonathan.chua@dollarshaveclub.com>
2016-08-09 17:18:44 -0700

    Added route for next-page.

---
+++ tests/unit/routes/next-page-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:next-page', 'Unit | Route | next page', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
